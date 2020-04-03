3cbcd6dd0d546c6ac4710a99bf4b581dfb5698df
Elton SV <el10savio96@gmail.com>
2020-04-03 21:44:33 +0530

    Refactored TranscribingDNAIntoRNA

--- TranscribingDNAIntoRNA/solution.go
+++ TranscribingDNAIntoRNA/solution.go
@@ -6,11 +6,12 @@ import (
 	definitions "github.com/el10savio/goRosalind/Definitions"
 )
 
-// Given:
+// Given: A DNA string t having length at 
+// most 1000 nt.
 //
-// Return:
+// Return: The transcribed RNA string of t.
 
-// Transcribe ...
+// Transcribe converts a DNA string to RNA
 func Transcribe(DNA string) (string, error) {
 	if len(DNA) <= 0 {
 		return "", definitions.ErrEmptyString
