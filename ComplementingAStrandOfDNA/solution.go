ad72a29ee35852814422a811bad2ad89385ddbbe
Elton SV <el10savio96@gmail.com>
2020-04-04 15:39:51 +0530

    Init ComplementingAStrandOfDNA Solution

--- ComplementingAStrandOfDNA/solution.go
+++ ComplementingAStrandOfDNA/solution.go
@@ -1,6 +1,8 @@
 package complementingastrandofdna
 
 import (
+	"strings"
+
 	definitions "github.com/el10savio/goRosalind/Definitions"
 )
 
@@ -14,5 +16,44 @@ func Complement(DNA string) (string, error) {
 		return "", definitions.ErrEmptyString
 	}
 
-	return "", nil
+	DNA = strings.ToUpper(DNA)
+	reversed := reverse(DNA)
+
+	complement, err := complement(reversed)
+	if err != nil {
+		return "", err
+	}
+
+	return complement, nil
+}
+
+func reverse(sentence string) string {
+	runes := []rune(sentence)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
+
+func complement(sentence string) (string, error) {
+	complements := map[rune]rune{
+		rune('A'): rune('T'),
+		rune('T'): rune('A'),
+		rune('C'): rune('G'),
+		rune('G'): rune('H'),
+	}
+
+	runes := []rune(sentence)
+
+	for _, rune := range runes {
+		if complement, ok := complements[rune]; !ok {
+			return "", definitions.ErrInvalidDNAString
+		}
+		rune = complements[rune]
+
+	}
+
+	return string(runes), nil
 }
--- Definitions/definitions.go
+++ Definitions/definitions.go
@@ -5,4 +5,7 @@ import "errors"
 var (
 	// ErrEmptyString is the error in case of empty string
 	ErrEmptyString = errors.New("string cannot be empty")
+
+	// ErrInvalidDNAString is the error in case of invalid DNA string
+	ErrInvalidDNAString = errors.New("invalid dna string")
 )
