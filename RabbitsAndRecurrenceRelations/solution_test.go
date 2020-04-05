be82c44f9b55e77795794010c1661d418d6cfbad
Elton SV <el10savio96@gmail.com>
2020-04-05 11:50:43 +0530

    Init RabbitsAndRecurrenceRelations Tests

--- RabbitsAndRecurrenceRelations/solution_test.go
+++ RabbitsAndRecurrenceRelations/solution_test.go
@@ -2,23 +2,49 @@ package rabbitsandrecurrencerelations
 
 import (
 	"testing"
+
+	definitions "github.com/el10savio/goRosalind/Definitions"
+	"github.com/stretchr/testify/assert"
 )
 
 type testCase struct {
 	Description string
+	Months      int
+	Litter      int
+	Total       int
 	Error       error
 }
 
 var testCases = []testCase{
-	testCase{},
+	testCase{
+		Description: "0 Total() Parameters",
+		Months:      0,
+		Litter:      0,
+		Total:       0,
+		Error:       definitions.ErrNotPositiveValue,
+	},
+	testCase{
+		Description: "Negative Total() Parameters",
+		Months:      0,
+		Litter:      0,
+		Total:       0,
+		Error:       definitions.ErrNotPositiveValue,
+	},
+	testCase{
+		Description: "Basic Total() Parameters",
+		Months:      5,
+		Litter:      3,
+		Total:       19,
+		Error:       nil,
+	},
 }
 
-func TestTranscribe(t *testing.T) {
+func TestTotal(t *testing.T) {
 	for _, tC := range testCases {
 		t.Run(tC.Description, func(t *testing.T) {
-			// RNAActual, ErrorActual := Transcribe(tC.DNA)
-			// assert.Equal(t, tC.Error, ErrorActual)
-			// assert.Equal(t, tC.RNA, RNAActual)
+			TotalActual, ErrorActual := Total(tC.Months, tC.Litter)
+			assert.Equal(t, tC.Error, ErrorActual)
+			assert.Equal(t, tC.Total, TotalActual)
 		})
 	}
 }
