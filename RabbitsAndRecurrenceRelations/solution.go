db6307ac2377fe7250d4b1ff0a856e10053b5b4b
Elton SV <el10savio96@gmail.com>
2020-04-05 12:07:46 +0530

    Init RabbitsAndRecurrenceRelations Solution

--- RabbitsAndRecurrenceRelations/solution.go
+++ RabbitsAndRecurrenceRelations/solution.go
@@ -14,5 +14,21 @@ func Total(months int, litter int) (int, error) {
 		return 0, definitions.ErrNotPositiveValue
 	}
 
-	return 0, nil
+	return fibonacci(months, litter), nil
+}
+
+func fibonacci(n int, init int) int {
+	if n == 1 {
+		return 1
+	}
+	
+	if n == 2 {
+		return init
+	}
+
+	if n <= 4 {
+		return fibonacci(n-1, init) + fibonacci(n-2, init)
+	}
+
+	return fibonacci(n-1, init) + fibonacci(n-2, init)*init
 }
