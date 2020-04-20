22d65c2249e590aa7394b72eb5f8ed3bf103d702
Elton SV <el10savio96@gmail.com>
2020-04-20 10:11:26 +0530

    Init ComputingGCContent Solution

--- ComputingGCContent/solution.go
+++ ComputingGCContent/solution.go
@@ -1,6 +1,8 @@
 package computinggccontent
 
 import (
+	"strings"
+
 	definitions "github.com/el10savio/goRosalind/Definitions"
 )
 
@@ -9,10 +11,30 @@ import (
 // Return:
 
 // Compute
-func Compute(DNA string) (string, error) {
+func Compute(label string, DNA string) (string, error) {
 	if len(DNA) <= 0 {
 		return "", definitions.ErrEmptyString
 	}
 
+	if len(label) <= 0 {
+		return "", definitions.ErrEmptyLabel
+	}
+
+	DNA = strings.ToLower(DNA)
+	gcContent := GCContent(DNA)
+
 	return "", nil
+}
+
+func GCContent(DNA string) float64 {
+	gcCount := 0
+	runes := []rune(DNA)
+
+	for _, runeValue := range runes {
+		if runeValue == rune('G') || runeValue == rune('C') {
+			gcCount++
+		}
+	}
+
+	return float64(gcCount / len(runes))
 }
